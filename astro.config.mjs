58a0e6b0e344418cac99d69ba3a9bfc1f8c367cb
carlos yonusg <cyonusg@carloss-MacBook-Pro.local>
2025-05-27 14:47:13 -0400

    feat: SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [mdx()]
+  integrations: [mdx(), sitemap()]
 });
